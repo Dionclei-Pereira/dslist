fc5aac07f08a00452ceb16d2cbfa0a0332e3ec81
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-01-25 11:38:12 -0300

    Initial commit

---
+++ src/test/java/me/dionclei/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dionclei.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
